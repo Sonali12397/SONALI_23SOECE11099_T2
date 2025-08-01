b8db716857e59602a399c386fb6e49349bb40a5b
Sonali Thakur <sonalithakur0808@gmail.com>
2025-08-01 15:37:06 +0530

    one

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Declare and initialize an array of integers
+        int[] numbers = new int[5];
+
+        // Input: Ask user to enter 5 elements
+        Console.WriteLine("Enter 5 integer values:");
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write($"Element [{i}]: ");
+            numbers[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        // Output: Print elements of the array
+        Console.WriteLine("\nThe elements in the array are:");
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine($"Element at index {i}: {numbers[i]}");
+        }
+    }
+}
